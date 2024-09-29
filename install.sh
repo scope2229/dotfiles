4b8c407dfbaa5e1a0baebd0b6f1ed9038ff385e2
scope2229 <kaine_76@hotmail.com>
2024-09-29 22:21:43 +0800

    install snippets

--- install.sh
+++ install.sh
@@ -7,3 +7,17 @@ else
 fi
 
 cat ~/dotfiles/bash/aliases > "${HOME}/.bash_aliases"
+
+echo "Install snippets for vscode..."
+# Define the target directory for VSCode snippets
+VSCODE_SNIPPETS_DIR="$HOME/.config/Code/User/snippets"
+
+# Check if the directory exists, if not create it
+if [ ! -d "$VSCODE_SNIPPETS_DIR" ]; then
+    mkdir -p "$VSCODE_SNIPPETS_DIR"
+fi
+
+# Copy the html.json file from the dotfiles repo to the VSCode snippets directory
+cp ~/dotfiles/vscode/snippets/html.json "$VSCODE_SNIPPETS_DIR/"
+
+echo "HTML snippets installed successfully!"
