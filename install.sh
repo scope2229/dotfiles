367950812a311175ee43160ab148f494c5365292
scope2229 <kaine_76@hotmail.com>
2024-10-10 12:48:57 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -68,13 +68,11 @@ echo "Installing Ruby version: $RUBY_VERSION; WITH GEMSET: $RUBY_GEMSET"
 #     echo "Waiting for RVM GEMSET $RUBY_GEMSET to be ready..."
 #     sleep 2
 # done
-/bin/bash --login
+exec bash --login
 
 source /usr/local/rvm/scripts/rvm
 
 rvm install $RUBY_VERSION
-
-cd .
 
 rvm use $RUBY_VERSION --default
 
